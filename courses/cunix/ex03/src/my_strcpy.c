d16c23225dedc0fc3095bbcce4a7d561b30e865d
andrew <andrey058@hitech18.online>
2021-08-12 22:08:09 +0300

    Solved task ex03

---
+++ courses/cunix/ex03/src/my_strcpy.c
@@ -0,0 +1,15 @@
+#include "test.h"
+
+char *my_strcpy(char *dest, const char *src)
+{
+
+    char *tmp = dest;
+    while (*src != '\0')
+    {
+        *dest = *src;
+        dest++;
+        src++;
+    }
+    *dest = '\0';
+    return tmp;
+}
