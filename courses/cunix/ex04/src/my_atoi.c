7b50cf295541d55a8bbf75587d31abeb93e5aecf
andrew <andrey058@hitech18.online>
2021-08-12 23:01:15 +0300

    Solved task ex04

---
+++ courses/cunix/ex04/src/my_atoi.c
@@ -0,0 +1,21 @@
+#include "test.h"
+
+int my_atoi(const char *nptr)
+{
+    int res = 0, sign = 1;
+
+    if (*nptr == '-')
+    {
+        sign = -1;
+        nptr++;
+    }
+
+    while (*nptr != '\0')
+    {
+        if (*nptr < '0' || *nptr > '9')
+            break;
+        res = res * 10 + (*nptr - '0');
+        nptr++;
+    }
+    return res * sign;
+}
