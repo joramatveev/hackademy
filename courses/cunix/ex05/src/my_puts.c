d3a7e9ca83f938fd4e247302351c38715e083dbf
andrew <andrey058@hitech18.online>
2021-08-12 23:02:17 +0300

    Solved task ex05

---
+++ courses/cunix/ex05/src/my_puts.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+int my_puts(const char *s)
+{
+    char new_line = '\n';
+    for (int i = 0; s[i] != '\0'; i++)
+    {
+        write(1, &s[i], sizeof(char));
+    }
+    write(1, &new_line, sizeof(char));
+
+    return 1;
+}
